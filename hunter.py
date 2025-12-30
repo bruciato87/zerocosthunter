0088f00800f96f7610b120f77b615dc646037c85
bruciato87 <papiandrea@gmail.com>
2025-12-30 13:39:47 +0100

    fix: add missing import requests in hunter.py

--- hunter.py
+++ hunter.py
@@ -10,6 +10,7 @@ if hasattr(ssl, '_create_unverified_context'):
     ssl._create_default_https_context = ssl._create_unverified_context
 
 import trafilatura # Added for Full-Text Scraping
+import requests # Added for Anti-Bot Headers
 
 # ... imports ...
 
